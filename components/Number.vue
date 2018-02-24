1e816b180ddb0a1459ef70c605801bb2a62de269
Nick Cotton <nick.c.cotton@gmail.com>
2018-02-24 23:18:07 +0000

    tidy up

--- components/Number.vue
+++ components/Number.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="inline-block mx-1 px-4 py-3 border border-grey rounded text-2xl hover:bg-grey-lighter cursor-pointer">
+  <div class="inline-block mx-1 px-4 py-3 border border-grey rounded text-2xl hover:bg-grey-lighter cursor-pointer select-none">
     <slot></slot>
   </div>
 </template>
--- pages/start.vue
+++ pages/start.vue
@@ -13,34 +13,17 @@
       <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-sm lg:flex-grow">
           <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-purple-lightest hover:text-white mr-4">
-            Docs
+            About
           </a>
-          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-purple-lightest hover:text-white mr-4">
-            Examples
-          </a>
-          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-purple-lightest hover:text-white">
-            Blog
-          </a>
-        </div>
-        <div>
-          <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-purple hover:bg-white mt-4 lg:mt-0">Download</a>
         </div>
       </div>
     </nav>
 
     <section class="container mx-auto">
-      <h2
-      v-if="steps[currentStep] != undefined"
-      class="text-center text-2xl md:text-3xl mb-4"
-      >{{ steps[currentStep].title }}</h2>
-
-      <p class="text-center text-xl mb-4">How would you rate this area of your life?</p>
-
-      <div class="text-center mb-4 lg:mb-8">
-        <Number v-for="score in 10" @click.native="submitScore(score)">{{ score }}</Number>
-      </div>
-
-      <svg :width="width" :height="height">
+      <svg
+      :width="width"
+      :height="height"
+      id="wheel">
         <g :transform="`translate(${width / 2}, ${height / 2})`">
 
           <path
@@ -68,6 +51,82 @@
       </svg>
     </section>
 
+    <section class="container mx-auto" v-if="steps[currentStep] != undefined">
+      <h2
+      class="text-center text-2xl md:text-3xl mb-4"
+      >{{ steps[currentStep].title }}</h2>
+
+      <p class="text-center text-xl mb-4">How would you rate this area of your life?</p>
+
+      <div class="text-center mb-4 lg:mb-8">
+        <Number v-for="score in 10" @click.native="submitScore(score)">{{ score }}</Number>
+      </div>
+    </section>
+
+    <transition name="fade">
+      <section class="container mx-auto" v-if="steps[currentStep] == undefined">
+        <h2
+        class="text-center text-2xl md:text-3xl mb-4"
+        >Done!</h2>
+
+        <p class="text-center text-xl mb-4">Have your wheel emailed to you.</p>
+
+        <form class="w-full max-w-sm mx-auto">
+          <div class="flex items-center border-b border-b-2 border-purple py-2">
+            <input class="appearance-none bg-transparent border-none w-full text-grey-darker mr-3 py-1 px-2" type="email" placeholder="Email address" aria-label="Email address">
+            <button class="flex-no-shrink bg-purple hover:bg-purple-light border-purple hover:border-purple-light text-sm border-4 text-white py-1 px-2 rounded" type="button">
+              Send
+            </button>
+          </div>
+        </form>
+      </section>
+    </transition>
+
+    <!-- <transition name="fade">
+      <section class="container mx-auto" v-if="steps[currentStep] == undefined">
+        <h2
+        class="text-center text-2xl md:text-3xl mb-4"
+        >Done!</h2>
+
+        <p class="text-center text-xl mb-4">Download your wheel or have it emailed to you.</p>
+
+        <div class="flex -mx-4">
+          <div class="w-1/4 px-4">
+            <div class="p-4 bg-grey-lighter">
+              <h3 class="font-bold text-xl mb-2">Email</h3>
+              <p class="text-grey-darker text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+          <div class="w-1/4 px-4">
+            <div class="p-4 bg-grey-lighter">
+              <h3 class="font-bold text-xl mb-2">Download</h3>
+              <p class="text-grey-darker text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+          <div class="w-1/4 px-4">
+            <div class="p-4 bg-grey-lighter">
+              <h3 class="font-bold text-xl mb-2">Share</h3>
+              <p class="text-grey-darker text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+          <div class="w-1/4 px-4">
+            <div class="p-4 bg-grey-lighter">
+              <h3 class="font-bold text-xl mb-2">Print</h3>
+              <p class="text-grey-darker text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </transition> -->
+
   </div>
 </template>
 
@@ -75,6 +134,7 @@
 import Logo from '~/components/Logo.vue'
 import Number from '~/components/Number.vue'
 import * as d3 from 'd3'
+import * as saveSvgAsPng from 'save-svg-as-png'
 // import TWEEN from 'tween.js'
 
 export default {
@@ -224,6 +284,10 @@ export default {
       this.something()
     },
 
+    savePng: function () {
+      saveSvgAsPng.saveSvgAsPng(document.getElementById('wheel'), 'diagram.png')
+    },
+
     something: function () {
       const that = this
 
@@ -233,10 +297,6 @@ export default {
         d.score = +d.score
         d.width = +d.weight
       })
-
-      // var pie = d3.pie()
-      //   .sort(null)
-      //   .value(function (d) { return d.weight })
 
       var segments = this.steps.map(function (d) { return d.weight })
 
@@ -258,23 +318,12 @@ export default {
         that.steps[i].textTranslateValue.x = (that.radius - 12) * Math.sin(((arcItem.endAngle - arcItem.startAngle) / 2) + arcItem.startAngle)
 
         that.steps[i].textTranslateValue.y = (-1 * (that.radius - 12) * Math.cos(((arcItem.endAngle - arcItem.startAngle) / 2) + arcItem.startAngle))
-
-        // var testArc = d3.arc()
-        //   .innerRadius(25)
-        //   .outerRadius(250)
-        //   .startAngle(5.497787143782138)
-        //   .endAngle(6.283185307179586)
-
-        // console.log(testArc())
       })
     }
 
   },
 
   mounted () {
-    // window.addEventListener('resize', this.onResize)
-    // this.onResize()
-
     this.radius = Math.min(this.width, this.height) / 2
 
     this.innerRadius = 0.1 * this.radius
@@ -283,70 +332,11 @@ export default {
 
     window.addEventListener('resize', this.onResize)
     this.onResize()
-
-    // var pie = d3.pie()
-    //   .sort(null)
-    //   .value(function (d) { return d.width })
-
-    // var arc = d3.arc()
-    //   .innerRadius(innerRadius)
-    //   .outerRadius(function (d) {
-    //     return (radius - innerRadius) * (d.data.score / 100.0) + innerRadius
-    //   })
-
-    // var svg = d3.select('svg g')
-    // .attr('width', width)
-    // .attr('height', height)
-    // .append('g')
-    // .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
-
-    // this.steps.forEach(function (d) {
-    //   d.color = d.color
-    //   d.weight = +d.weight
-    //   d.score = +d.score
-    //   d.width = +d.weight
-    // })
-    // for (var i = 0; i < data.score; i++) { console.log(data[i].id) }
-
-    // eslint-disable-next-line no-unused-vars
-    // var path = d3.select('svg g')
-    //   .selectAll('.solidArc')
-    //   .data(pie(this.steps))
-    //   .enter().append('path')
-    //   .attr('fill', function (d) { return d.data.color })
-    //   .attr('class', 'solidArc')
-    //   .attr('stroke', 'gray')
-    //   .attr('d', d3.arc()
-    //     .innerRadius(this.innerRadius)
-    //     .outerRadius(function (d) {
-    //       return (this.radius - this.innerRadius) * (d.data.score / 100.0) + this.innerRadius
-    //     })
-    //   )
-
-    // // calculate the weighted mean score
-    // var score =
-    //   this.steps.reduce(function (a, b) {
-    //     // console.log('a:' + a + ', b.score: ' + b.score + ', b.weight: ' + b.weight);
-    //     return a + (b.score * b.weight)
-    //   }, 0) /
-    //   this.steps.reduce(function (a, b) {
-    //     return a + b.weight
-    //   }, 0)
-
-    // svg.append('svg:text')
-    //   .attr('class', 'aster-score')
-    //   .attr('dy', '.35em')
-    //   .attr('text-anchor', 'middle') // text-align: right
-    //   .text(Math.round(score))
   }
 }
 </script>
 
 <style>
-  body {
-    font: 10px sans-serif;
-  }
-
   .axis path,
   .axis line {
     fill: none;
@@ -358,15 +348,11 @@ export default {
     fill: orange;
   }
 
-  .solidArc:hover {
-    fill: orangered ;
-  }
-
   .solidArc {
-      -moz-transition: all 0.3s;
-      -o-transition: all 0.3s;
-      -webkit-transition: all 0.3s;
-      transition: all 0.3s;
+    -moz-transition: all 0.3s;
+    -o-transition: all 0.3s;
+    -webkit-transition: all 0.3s;
+    transition: all 0.3s;
   }
 
   .x.axis path {
