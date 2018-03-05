3804d8cbef777d9a47ef1d50be6322b0b6fd08c9
Nick Cotton <nick.c.cotton@gmail.com>
2018-03-05 20:23:45 +0000

    WIP index pg

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
-    <h1>Wheel of Life</h1>
+    <div class="header flex items-center h-screen">
+      <section class="container mx-auto text-center">
+        <div class="bg-white p-4 inline-block">
+          <h1>Wheel of Life</h1>
 
-    <button @click="$router.push('/start')" class="bg-blue mt-4 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded">Start</button>
+          <button @click="$router.push('/start')" class="bg-purple mt-4 hover:bg-purple-dark text-white font-bold py-2 px-4 rounded">Start</button>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
+<style scoped>
+  .header {
+    padding: 400px 0;
+    background-image: url(~/assets/images/flower.jpg);
+    background-size: cover;
+    background-position: 50% 50%;
+  }
+</style>
